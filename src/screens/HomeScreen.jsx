85458a4bab2f6c398fd784bce8ec0350dfbc3aa0
Luccas Gomes <luccascorreia.08@hotmail.com>
2023-11-21 16:29:18 -0300

    first commit

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+
+export default function HomeScreen() {
+  return (
+    <Container
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>Home Screen</h1>
+    </Container>
+  );
+}
